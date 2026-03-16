0ec00215605e7af14e4a07e946a519cbd5c529fa
newtime595 <newtime595@gmail.com>
2026-03-16 17:53:42 +0900

    260316 5:53 홍성인 admin 수정 실패

---
+++ Oulim/admin/js/member-manage/mem-detail.js
@@ -0,0 +1,8 @@
+
+const pointNum = document.getElementsByClassName(".point-count");
+
+pointNum.addEventListener("click", function(){
+  location.href = "/Oulim/admin/html/member-manage/mem-edit.html";
+});s
+
+
